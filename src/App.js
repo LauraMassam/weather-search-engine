1703a5a5fbb63456c4d1440655b3105579d52f09
LauraMassam <68440423+LauraMassam@users.noreply.github.com>
2021-08-15 18:33:31 +0100

    added axios & weather API integration (added CCS to these)

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <Weather />
+      <Weather defaultCity = "London"/>
       </div>
       <footer>
         open-sourced code by <a href ="https://github.com/LauraMassam/weather-search-engine" target="_blank" rel="noreferrer">
--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
 import "./Weather.css"; 
+import { mixedTypeAnnotation } from "@babel/types";
 
-export default function Weather(){
+export default function Weather(props){
+    const [weatherData, setWeatherData] = useState ({ready : false});
+    function handleResponse (response) {
+        console.log(response.data);
+        setWeatherData ({
+            ready : true,
+            city: response.data.name,
+            date: "Saturday 15:38",
+            description: response.data.weather[0].description,
+            feelsLike: Math.round(response.data.main.feels_like),
+            humidity: Math.round(response.data.main.humidity),
+            iconUrl: "https://openweathermap.org/img/wn/02d@2x.png",
+            temperature: Math.round(response.data.main.temp),
+            sunrise: response.data.sys.sunrise,
+            sunset: response.data.sys.sunset,
+            tempMax: Math.round(response.data.main.temp_max),
+            tempMin: Math.round(response.data.main.temp_min),
+            wind: Math.round(response.data.wind.speed),
+    });
+    }
+    if (weatherData.ready) {
     return(
 <div>
     <div className="row search-form">
@@ -16,47 +38,68 @@ export default function Weather(){
     </h6>
     <div className="row">
         <div className="col-8">
-            <h1>
-                 London
+            <h1 className = "text-capitalize">
+                 {weatherData.city}
             </h1>
         </div>
         <div className="col-4">
             <h3>
-                 Saturday
-                 15:38
+                 {weatherData.date}
             </h3>
         </div>
 
         <div className="row">
             <div className="col-6">
-                <img src = "https://openweathermap.org/img/wn/02d@2x.png"
-                     alt = "weather"
+                <img src = {weatherData.iconUrl}
+                     alt = {weatherData.description}
                      style = {{width: 150}}
                 />
-                <div className="row humidity-wind">
-                    humidity: 72%
-                    Wind: 3km/h
-                </div>
-                
             </div>
             <div className="col-6 temperature">
-                <ul>
-                <li>
-                    <span className = "current-temperature">17</span> 
-                    <span className = "temp-measurements">°C | F </span> 
-                </li>
-                <li className = "high-low-temp">16° | <strong>19</strong>°</li>
-                <li>feels like</li>
-                <li>
-                <div className="row sunrise-sunset">
-                    <div className="col"> sunrise: 06:00 </div>
-                    <div className="col">sunset: 21:48</div>
-                </div>
-                </li>
-                </ul>
+                <span className = "current-temperature">
+                    {weatherData.temperature}
+                </span>
+                <span className = "temp-measurements">
+                    °C | F 
+                </span>             
+                
+                    <div className = "high-low-temp">
+                        {weatherData.tempMin}° | {""}
+                        <strong>
+                        {weatherData.tempMax}
+                        </strong>
+                        °
+                    </div>
+                    <div className = "feels-like">
+
+                    feels like : {weatherData.feelsLike}
+                    </div>
             </div>
+
+            <div className="row weather-details">
+                <div className="col">
+                        humidity: {weatherData.humidity}%
+                </div>
+                <div className="col">
+                        Wind: {weatherData.wind}km/h
+                </div>
+                <div className="col">
+                        sunrise: {weatherData.sunrise}
+                </div>
+                <div className="col">
+                sunset: {weatherData.sunset}
+                </div>
+            </div>
+
         </div>
     </div>
 </div>
-      )
+      );
+      
+} else {
+    const apiKey ="9aaa9a2a183bbe9e6cb58bc031908f93";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+    return "loading..."
+}
 }
